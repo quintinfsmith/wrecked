97c0cec1955b76bc8422a1c88e990b8fe4094e7a
Quintin Smith <smith.f.quintin@gmail.com>
2019-02-23 17:36:32 -0800

    Added switching to a new screen on init and kill. Created Parallel python objects

--- src/control.py
+++ src/control.py
@@ -1,34 +1,98 @@
 from cffi import FFI
 from ctypes import c_bool
 import sys
-ffi = FFI()
+import tty, termios
+import os
 
-ffi.cdef("""
-    typedef void* BleepsBoxes;
+def get_terminal_size():
+    height, width = os.popen('stty size', 'r').read().split()
+    return (int(width), int(height))
 
-    BleepsBoxes init();
+class BleepsScreen(object):
+    SO_PATH = "/home/pent/Projects/AsciiBox/target/debug/libasciibox.so"
+    def __init__(self):
+        ffi = FFI()
+        ffi.cdef("""
+            typedef void* BleepsBoxes;
 
-    uint32_t newbox(BleepsBoxes, uint32_t);
+            BleepsBoxes init(uint32_t, uint32_t);
 
-    void flag_recache(BleepsBoxes, uint32_t);
-    void draw(BleepsBoxes);
-    void setc(BleepsBoxes, uint32_t, uint32_t, uint32_t, const char*);
-    void printc(BleepsBoxes, uint32_t, uint32_t, uint32_t);
+            uint32_t newbox(BleepsBoxes, uint32_t, uint32_t, uint32_t);
 
-""")
+            void movebox(BleepsBoxes, uint32_t, uint32_t, uint32_t);
+            void flag_recache(BleepsBoxes, uint32_t);
+            void set_bg_color(BleepsBoxes, uint32_t, uint8_t);
+            void set_fg_color(BleepsBoxes, uint32_t, uint8_t);
+            void setc(BleepsBoxes, uint32_t, uint32_t, uint32_t, const char*);
+            void draw(BleepsBoxes);
+            void kill(BleepsBoxes);
+        """)
 
-lib = ffi.dlopen("/home/pent/Projects/AsciiBox/target/debug/libasciibox.so")
+        self.lib = ffi.dlopen(self.SO_PATH)
+        w, h = get_terminal_size()
+        self.box_vector = self.lib.init(w, h)
+
+    def box_setc(self, box_id, x, y, character):
+        fmt_character = bytes(character, 'utf-8')
+        self.lib.setc(self.box_vector, box_id, x, y, fmt_character)
+    def box_move(self, box_id, x, y):
+        self.lib.movebox(self.box_vector, box_id, x, y)
+
+    def _new_box(self, width, height, parent=0):
+
+        new_box_id = self.lib.newbox(self.box_vector, parent, width, height)
+        return BleepsBox(new_box_id, width, height, self)
+
+    def new_box(self, width, height):
+        return self._new_box(width, height)
 
 
+    def draw(self):
+        self.lib.draw(self.box_vector)
+
+    def kill(self):
+        self.lib.kill(self.box_vector)
 
 
-screen = lib.init()
-box = lib.newbox(screen, 0)
-box = lib.newbox(screen, box)
-lib.setc(screen, box, 3, 3, b'q')
-#lib.printc(screen, 1, 3, 3)
+class BleepsBox(object):
+    def __init__(self, n, width, height, screen):
+        self._screen  = screen
+        self.bleeps_id = n
+        self.boxes = {}
+        self.width = width
+        self.height = height
 
-lib.draw(screen)
+    def setc(self, x, y, character):
+        self._screen.box_setc(self.bleeps_id, x, y, character)
+
+    def move(self, new_x, new_y):
+        self._screen.box_move(self.bleeps_id, new_x, new_y)
+
+    def set_fg_color(self, new_col):
+        self._screen.box_set_fg_color(self.bleeps_id, new_col)
+
+    def set_bg_color(self, new_col):
+        self._screen.box_set_bg_color(self.bleeps_id, new_col)
+
+    def new_box(self, width, height):
+        box = self._screen._new_box(width, height, self.bleeps_id)
+        self.boxes[box.id] = box
+
+        return box
 
 
+screen = BleepsScreen()
+box = screen.new_box(10, 10)
+for y in range(box.height):
+    box.setc(0, y, '|')
+    box.setc(box.width - 1, y, '|')
 
+for x in range(box.width):
+    box.setc(x, 0, '=')
+    box.setc(x, box.height - 1, '=')
+
+import time
+screen.draw()
+
+time.sleep(2)
+screen.kill()
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ use std::os::raw::c_char;
 use std::collections::HashMap;
 use std::cmp;
 use std::io::{self, Write};
-
+use std::slice;
+use std::str;
 
 fn write(towrite: &[u8]) -> io::Result<()> {
     let stdout = io::stdout();
@@ -15,19 +16,21 @@ fn write(towrite: &[u8]) -> io::Result<()> {
 
 pub struct BleepsBox {
     boxes: Vec<usize>,
-    box_positions: Vec<(isize, isize)>,
+    box_positions: HashMap<usize, (isize, isize)>,
     width: usize,
     height: usize,
-    grid: Vec<Vec<char>>,
-    cached: Vec<((isize, isize), (char, u16))>,
+    grid: Vec<Vec<[u8; 4]>>,
+    cached: Vec<((isize, isize), ([u8; 4], u16))>,
     parent: Option<usize>,
-    recache_flag: bool
+
+    recache_flag: bool,
+    color: u16, // 7: USE FG, 6-4: FG, 3: USEBG, 2-0:BG
 }
 
 impl BleepsBox {
     fn new(width: usize, height: usize) -> BleepsBox {
         let mut newgrid = Vec::new();
-        let mut newrow: Vec<Option<char>>;
+        let mut newrow: Vec<Option<[u8; 4]>>;
         for y in (0 .. height) {
             newrow = Vec::new();
             for x in (0 .. width) {
@@ -37,46 +40,65 @@ impl BleepsBox {
         }
         BleepsBox {
             boxes: Vec::new(),
-            box_positions: Vec::new(),
+            box_positions: HashMap::new(),
             width: width,
             height: height,
             grid: Vec::new(),
             cached: Vec::new(),
             parent: None,
-            recache_flag: true
+            recache_flag: true,
+            color: 0
         }
     }
     fn flag_recache(&mut self) {
         self.recache_flag = true;
     }
-    fn set(&mut self, x: usize, y: usize, c: char) {
+    fn set(&mut self, x: usize, y: usize, c: &[u8]) {
+        let mut new_c: [u8; 4] = [0; 4];
+        for i in 0..c.len() {
+            new_c[(4 - c.len()) + i] = c[i]; // Put the 0 offset first
+        }
         while (self.grid.len() <= y) {
             self.grid.push(Vec::new());
         }
         match self.grid.get_mut(y) {
             Some(row) => {
                 while (row.len() <= x) {
-                    row.push(' ');
+                    row.push([0u8; 4]);
                 }
-                row[x] = c;
+                row[x] = new_c;
             }
             None => ()
         };
     }
 
-    fn get(&self, x: usize, y: usize) -> Option<char> {
-        if y < self.grid.len() && x < self.grid.len() {
+    fn set_bg_color(&mut self, n: u8) {
+        let mut modded_n: u16 = n as u16;
+        modded_n &= 0b01111;
+        modded_n |= 0b10000;
+        self.color |= modded_n;
+    }
+
+    fn set_fg_color(&mut self, n: u8) {
+        let mut modded_n: u16 = n as u16;
+        modded_n &= 0b01111;
+        modded_n |= 0b10000;
+        self.color |= (modded_n << 5)
+    }
+
+    fn get(&self, x: usize, y: usize) -> Option<[u8; 4]> {
+        if y < self.grid.len() && x < self.grid[y].len() {
             Some(self.grid[y][x])
         } else {
             None
         }
     }
 
-    fn get_cached(&self) -> Vec<((isize, isize), (char, u16))> {
+    fn get_cached(&self) -> Vec<((isize, isize), ([u8; 4], u16))> {
         self.cached.clone()
     }
 
-    fn set_cached(&mut self, tocache: &Vec<((isize, isize), (char, u16))>) {
+    fn set_cached(&mut self, tocache: &Vec<((isize, isize), ([u8; 4], u16))>) {
         self.cached = (*tocache).clone();
         self.recache_flag = false;
     }
@@ -89,21 +111,21 @@ fn rects_intersect(rect_a: (isize, isize, isize, isize), rect_b: (isize, isize, 
 }
 
 
-fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u16))> {
+fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),([u8; 4], u16))> {
     // box id, xoffset, yoffset
     let mut process_stack: Vec<(usize, (isize, isize))> = Vec::new();
     process_stack.push((0, (0, 0)));
-
     // Amalgamation of BleepsBoxes
-    let mut main_disp: Vec<((isize, isize), (char, u16))>;
+    let mut main_disp: Vec<((isize, isize), ([u8; 4], u16))>;
     main_disp = Vec::new();
 
     // Content displayed within 'current' BleepsBox
-    let mut tmp_disp: Vec<((isize, isize), (char, u16))>;
+    let mut tmp_disp: Vec<((isize, isize), ([u8; 4], u16))>;
 
-    let mut used_coords: HashMap<(isize, isize), (char, u16)> = HashMap::new();
 
+    let mut used_coords: HashMap<(isize, isize), ([u8; 4], u16)> = HashMap::new();
     let mut children_stacked: Vec<usize> = Vec::new();
+
 
     let mut current_bleepsbox: &mut BleepsBox;
     let mut current_id: usize;
@@ -111,7 +133,7 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
     let mut current_offset: (isize, isize);
 
     let mut pos: (isize, isize);
-    let mut val: (char, u16);
+    let mut val: ([u8; 4], u16);
     let mut new_position: (isize, isize);
 
     while process_stack.len() > 0 {
@@ -119,7 +141,7 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
         current_id = current_tuple.0;
         current_offset = current_tuple.1;
         match box_handler.get_mut(current_id) {
-				    Some(current_bleepsbox) => {
+            Some(current_bleepsbox) => {
                 // If the display has been cached on this box, don't descend
                 if current_bleepsbox.recache_flag && current_bleepsbox.boxes.len() > 0 && ! children_stacked.contains(&current_id) {
                     // Reinsert id to stack
@@ -127,12 +149,13 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
 
                     // Add Children to stack
                     for i in 0..current_bleepsbox.boxes.len() {
+                        let mut child_id = current_bleepsbox.boxes[i];
                         pos = (
-                            current_offset.0 + current_bleepsbox.box_positions[i].0,
-                            current_offset.1 + current_bleepsbox.box_positions[i].1
-                        );
+                                current_offset.0 + current_bleepsbox.box_positions[&child_id].0,
+                                current_offset.1 + current_bleepsbox.box_positions[&child_id].1
+                              );
 
-                        process_stack.push((current_bleepsbox.boxes[i], pos));
+                        process_stack.push((child_id, pos));
                     }
 
                     // Mark that children have been added to stack
@@ -148,14 +171,14 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
                                     Some(_found) => {
                                         tmp_disp.push((real_pos, (_found.0, _found.1))); // TODO: Will Fail
                                     },
-                                    None => {
-                                        match current_bleepsbox.get(x, y) {
-                                            Some(value) => {
-                                                tmp_disp.push(((x as isize, y as isize), (value, 0)));
-                                            }
-                                            None => ()
-                                        };
-                                    }
+                                        None => {
+                                            match current_bleepsbox.get(x, y) {
+                                                Some(value) => {
+                                                    tmp_disp.push(((x as isize, y as isize), (value, current_bleepsbox.color as u16)));
+                                                }
+                                                None => ()
+                                            };
+                                        }
                                 };
                             }
                         }
@@ -172,9 +195,9 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
                         }
                     }
                 }
-						}
-					  None => ()
-				};
+            }
+            None => ()
+        };
     }
     main_disp
 }
@@ -182,20 +205,55 @@ fn get_display(box_handler: &mut Vec<BleepsBox>) -> Vec<((isize, isize),(char, u
 fn _draw_boxes(boxes: &mut Vec<BleepsBox>) {
     let top_disp = get_display(boxes);
     let mut pos: (isize, isize);
-    let mut val: (char, u16);
+    let mut val_a: &[u8];
+    let mut val_b: u16;
     let mut s;
+    let mut utf_char: &[u8];
+    let mut utf_char_split_index: usize;
 
     for i in 0..top_disp.len() {
         pos = top_disp[i].0;
-        val = top_disp[i].1;
-        s = format!("\x1B[{};{}H{}", pos.0 + 1, pos.1 + 1, val.0);
-        println!("{}", s);
-        //write(s.as_bytes());
+        val_a = &((top_disp[i].1).0);
+        val_b = (top_disp[i].1).1;
+        s = format!("\x1B[{};{}H", pos.1 + 1, pos.0 + 1);
+        print!("{}", s);
+        if (val_b >> 5) & 16 == 16 {
+            if (val_b >> 5) & 8 == 8 {
+                s = format!("\x1B[9{}m", ((val_b >> 5) & 7));
+                print!("{}", s);
+            } else {
+                s = format!("\x1B[3{}m", ((val_b >> 5) & 7));
+                print!("{}", s);
+            }
+        }
+
+        if val_b & 16 == 16 {
+            if (val_b & 8 == 8) {
+                s = format!("\x1B[10{}m", (val_b & 7));
+                print!("{}", s);
+            } else {
+                s = format!("\x1B[4{}m", (val_b & 7));
+                print!("{}", s);
+            }
+        }
+
+        utf_char_split_index = 0;
+        for i in 0..4 {
+            if (val_a[i] != 0) {
+                utf_char_split_index = i;
+                break;
+            }
+        }
+
+        utf_char = val_a.split_at(utf_char_split_index).1;
+
+        s = format!("{}\x1B[0m", str::from_utf8(utf_char).unwrap());
+        print!("{}", s);
     }
+    print!("\n");
 }
 
 fn _flag_recache(boxes: &mut Vec<BleepsBox>, box_id: usize) {
-
     let mut next_box_id: usize = box_id as usize;
     let mut prev_box_id: usize = 0;
 
@@ -219,28 +277,7 @@ fn _flag_recache(boxes: &mut Vec<BleepsBox>, box_id: usize) {
             }
         };
     }
-
 }
-
-
-//// printc for testing only
-//#[no_mangle]
-//pub extern "C" fn printc(ptr: *mut Vec<BleepsBox>, box_id: u32, x: u32, y: u32) {
-//    let mut boxes = unsafe { Box::from_raw(ptr) };
-//    match boxes.get(box_id as usize) {
-//        Some(bleepsbox) => {
-//            match bleepsbox.get(x as usize, y as usize) {
-//                Some(c) => {
-//                    println!("{}", c);
-//                }
-//                None => ()
-//            };
-//        }
-//        None => ()
-//    };
-//
-//    Box::into_raw(boxes); // Prevent Release
-//}
 
 #[no_mangle]
 pub extern "C" fn draw(ptr: *mut Vec<BleepsBox>) {
@@ -250,6 +287,7 @@ pub extern "C" fn draw(ptr: *mut Vec<BleepsBox>) {
     Box::into_raw(boxes); // Prevent Release
 }
 
+
 #[no_mangle]
 pub extern "C" fn flag_recache(ptr: *mut Vec<BleepsBox>, box_id: usize) {
     let mut boxes = unsafe { Box::from_raw(ptr) };
@@ -258,18 +296,50 @@ pub extern "C" fn flag_recache(ptr: *mut Vec<BleepsBox>, box_id: usize) {
     Box::into_raw(boxes); // Prevent Release
 }
 
+
+#[no_mangle]
+pub extern "C" fn set_fg_color(ptr: *mut Vec<BleepsBox>, box_id: usize, col: u8) {
+    let mut boxes = unsafe { Box::from_raw(ptr) };
+    match boxes.get_mut(box_id as usize) {
+        Some(bleepsbox) => {
+            bleepsbox.set_fg_color(col);
+        }
+        None => ()
+    };
+
+    _flag_recache(&mut boxes, box_id);
+
+    Box::into_raw(boxes); // Prevent Release
+}
+
+
+#[no_mangle]
+pub extern "C" fn set_bg_color(ptr: *mut Vec<BleepsBox>, box_id: usize, col: u8) {
+    let mut boxes = unsafe { Box::from_raw(ptr) };
+    match boxes.get_mut(box_id as usize) {
+        Some(bleepsbox) => {
+            bleepsbox.set_bg_color(col);
+        }
+        None => ()
+    };
+
+    _flag_recache(&mut boxes, box_id);
+
+    Box::into_raw(boxes); // Prevent Release
+}
+
+
 #[no_mangle]
 pub extern "C" fn setc(ptr: *mut Vec<BleepsBox>, box_id: usize, x: usize, y: usize, c: *const c_char) {
     assert!(!c.is_null());
 
     let c_str = unsafe { CStr::from_ptr(c) };
-    let string = c_str.to_str().expect("Not a valid UTF-8 string");
-    let use_c = string.chars().next().unwrap();
+    let string_bytes = c_str.to_str().expect("Not a valid UTF-8 string").as_bytes();
 
     let mut boxes = unsafe { Box::from_raw(ptr) };
     match boxes.get_mut(box_id as usize) {
         Some(bleepsbox) => {
-            bleepsbox.set(x as usize, y as usize, use_c);
+            bleepsbox.set(x as usize, y as usize, string_bytes);
         }
         None => ()
     };
@@ -280,34 +350,75 @@ pub extern "C" fn setc(ptr: *mut Vec<BleepsBox>, box_id: usize, x: usize, y: usi
 }
 
 #[no_mangle]
-pub extern "C" fn newbox(ptr: *mut Vec<BleepsBox>, parent_id: usize) -> usize {
+pub extern "C" fn newbox(ptr: *mut Vec<BleepsBox>, parent_id: usize, width: usize, height: usize) -> usize {
     let mut boxes = unsafe { Box::from_raw(ptr) };
     let id: usize = boxes.len();
-    let mut bleepsbox = BleepsBox::new(10, 10);
+    let mut bleepsbox = BleepsBox::new(width, height);
 
-    if boxes.len() > parent_id as usize {
+    if boxes.len() > parent_id {
         {
             let mut parent = &mut boxes[parent_id as usize];
-            parent.box_positions.push((0, 0));
+            parent.box_positions.insert(id, (0, 0));
             parent.boxes.push(id);
             bleepsbox.parent = Some(parent_id);
         }
         boxes.push(bleepsbox);
     }
 
-
     Box::into_raw(boxes); // Prevent Release
 
     id
 }
 
+#[no_mangle]
+pub extern "C" fn movebox(ptr: *mut Vec<BleepsBox>, box_id: usize, x: isize, y: isize) {
+
+    let mut boxes = unsafe { Box::from_raw(ptr) };
+
+    let parent_id: usize;
+
+    if boxes.len() > box_id  && box_id > 0 {
+        match boxes.get(box_id) {
+            Some(_found) => {
+                parent_id = _found.parent.unwrap();
+            }
+            None => {
+                parent_id = 0;
+            }
+        };
+        match boxes.get_mut(parent_id) {
+            Some(parent) => {
+                parent.flag_recache();
+                if let Some(pos) = parent.box_positions.get_mut(&box_id) {
+                    *pos = (x, y);
+                }
+            }
+            None => ()
+        }
+    }
+
+    Box::into_raw(boxes); // Prevent Release
+}
+
 
 #[no_mangle]
-pub extern "C" fn init() -> *mut Vec<BleepsBox> {
+pub extern "C" fn init(width: usize, height: usize) -> *mut Vec<BleepsBox> {
     let mut boxes: Vec<BleepsBox> = Vec::new();
-    let top: BleepsBox = BleepsBox::new(10, 10);
+    let top: BleepsBox = BleepsBox::new(width, height);
     boxes.push(top);
+
+    println!("\x1B[?1049h"); // New screen
+    println!("\x1B[?25l"); // Hide Cursor
 
     Box::into_raw(Box::new(boxes))
 }
 
+#[no_mangle]
+pub extern "C" fn kill(ptr: *mut Vec<BleepsBox>) {
+    let mut boxes = unsafe { Box::from_raw(ptr) };
+
+    println!("\x1B[?25h"); // Show Cursor
+    println!("\x1B[?1049l"); // Return to previous screen
+
+    // Releases boxes
+}
