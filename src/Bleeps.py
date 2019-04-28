b69856d314159e36f2e78961b689f8aabae900e6
Quintin Smith <smith.f.quintin@gmail.com>
2019-04-28 15:16:54 -0700

    ..

--- src/Bleeps.py
+++ src/Bleeps.py
@@ -95,7 +95,7 @@ class BleepsScreen(object):
     def box_move(self, box_id, x, y):
         self.lib.movebox(self.boxhandler, box_id, x, y)
 
-    def box_resize(self, box_id, width height):
+    def box_resize(self, box_id, width, height):
         self.lib.resize
 
     def _new_box(self, width, height, parent=0):
